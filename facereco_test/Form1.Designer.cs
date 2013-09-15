65b0ded0bab8c4b06877dcb5187c738517995baf
Alejando del Rio (a-delrio) <a.delrio.santiago@gmail.com>
2013-09-15 04:33:55 -0600

    First version of IPCSDK winform sample added
    This is the first version of the project. Contributions are welcome!

---
+++ facereco_test/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace facereco_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "On/Off";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(174, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(514, 386);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(15, 118);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Eyes closed";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(15, 86);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(59, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Smiling";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Gender";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Baby";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Not processing";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Enabled = false;
+            this.trackBar1.Location = new System.Drawing.Point(15, 154);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.TickFrequency = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Enabled = false;
+            this.trackBar2.Location = new System.Drawing.Point(15, 205);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.TickFrequency = 0;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Enabled = false;
+            this.trackBar3.Location = new System.Drawing.Point(15, 256);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 9;
+            this.trackBar3.TickFrequency = 0;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Enabled = false;
+            this.trackBar4.Location = new System.Drawing.Point(15, 307);
+            this.trackBar4.Maximum = 100;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(104, 45);
+            this.trackBar4.TabIndex = 10;
+            this.trackBar4.TickFrequency = 0;
+            this.trackBar4.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Enabled = false;
+            this.trackBar5.Location = new System.Drawing.Point(15, 358);
+            this.trackBar5.Maximum = 100;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(104, 45);
+            this.trackBar5.TabIndex = 11;
+            this.trackBar5.TickFrequency = 0;
+            this.trackBar5.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Toddler";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(125, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Youth";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(125, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Adult";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(125, 371);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Senior";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 410);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Face Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.CheckBox checkBox1;
+        public System.Windows.Forms.CheckBox checkBox2;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TrackBar trackBar1;
+        public System.Windows.Forms.TrackBar trackBar2;
+        public System.Windows.Forms.TrackBar trackBar3;
+        public System.Windows.Forms.TrackBar trackBar4;
+        public System.Windows.Forms.TrackBar trackBar5;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Label label5;
+        public System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ facereco_test/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/*
+ * Important notice: When you load up the project on Visual Studio the reference to libpxcclr may not be there.
+ * To solve this simply go to Project->Add reference and then search for the library (normally in C:\Program Files\Intel\PCSDK\bin\win32\libpxcclr.dll)
+*/
+
+namespace facereco_test
+{
+    public partial class Form1 : Form
+    {
+        MyPipeline pipeline;
+        bool isRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                isRunning = false;
+                pipeline.PauseFaceLocation(true);
+                pipeline.Close();
+            }
+            else
+            {
+                isRunning = true;
+                pipeline.LoopFrames();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pipeline = new MyPipeline(this, pictureBox1);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class MyPipeline : UtilMPipeline
+    {
+        //Core variables
+        ulong timeStamp;
+        int faceId;
+        uint fidx = 0; //Unknown variable, what does this does?
+
+        //Statuses
+        pxcmStatus locationStatus;
+        pxcmStatus landmarkStatus;
+        pxcmStatus attributeStatus;
+        public bool takeRecoSnapshot = false;
+
+        //Form variables
+        Form1 parent;
+        string detectionConfidence;
+        Bitmap lastProcessedBitmap;
+
+        //Attribute array
+        uint[] age_group = new uint[5];
+        uint[] gender = new uint[2];
+        uint[] blink = new uint[2];
+        uint[] smile = new uint[1];
+        
+        //PXCM variables
+        PXCMFaceAnalysis faceAnalysis;
+        PXCMSession session;
+        PXCMFaceAnalysis.Detection faceLocation;
+        PXCMFaceAnalysis.Landmark faceLandmark;
+        PXCMFaceAnalysis.Attribute faceAttributes;
+        PXCMFaceAnalysis.Detection.Data faceLocationData;
+        PXCMFaceAnalysis.Landmark.LandmarkData[] faceLandmarkData;
+        PXCMFaceAnalysis.Landmark.ProfileInfo landmarkProfile;
+        PXCMFaceAnalysis.Attribute.ProfileInfo attributeProfile;
+
+        //Face data
+        PictureBox recipient; //Where the image will be drawn
+
+        public MyPipeline(Form1 parent, PictureBox recipient)
+        {
+            lastProcessedBitmap = new Bitmap(640, 480);            
+
+            this.recipient = recipient;
+            this.parent = parent;
+                        
+            attributeProfile = new PXCMFaceAnalysis.Attribute.ProfileInfo();
+
+            EnableImage(PXCMImage.ColorFormat.COLOR_FORMAT_RGB24);
+            EnableFaceLocation();
+            EnableFaceLandmark();            
+        }
+
+        public override bool OnNewFrame()
+        {
+            faceAnalysis = QueryFace();
+            faceAnalysis.QueryFace(fidx, out faceId, out timeStamp);
+            
+            //Get face location
+            faceLocation = (PXCMFaceAnalysis.Detection)faceAnalysis.DynamicCast(PXCMFaceAnalysis.Detection.CUID);
+            locationStatus = faceLocation.QueryData(faceId, out faceLocationData);
+            detectionConfidence = faceLocationData.confidence.ToString();
+            parent.label1.Text = "Confidence: " + detectionConfidence;
+
+            //Get face landmarks (eye, mouth, nose position, etc)
+            faceLandmark = (PXCMFaceAnalysis.Landmark)faceAnalysis.DynamicCast(PXCMFaceAnalysis.Landmark.CUID);
+            faceLandmark.QueryProfile(1, out landmarkProfile);
+            faceLandmark.SetProfile(ref landmarkProfile);
+            faceLandmarkData = new PXCMFaceAnalysis.Landmark.LandmarkData[7];
+            landmarkStatus = faceLandmark.QueryLandmarkData(faceId, PXCMFaceAnalysis.Landmark.Label.LABEL_7POINTS, faceLandmarkData);
+
+            //Get face attributes (smile, age group, gender, eye blink, etc)
+            faceAttributes = (PXCMFaceAnalysis.Attribute)faceAnalysis.DynamicCast(PXCMFaceAnalysis.Attribute.CUID);
+            faceAttributes.QueryProfile(PXCMFaceAnalysis.Attribute.Label.LABEL_EMOTION, 0, out attributeProfile);
+            faceAttributes.SetProfile(PXCMFaceAnalysis.Attribute.Label.LABEL_EMOTION, ref attributeProfile);
+            attributeStatus = faceAttributes.QueryData(PXCMFaceAnalysis.Attribute.Label.LABEL_EMOTION, faceId, out smile);
+
+            faceAttributes.QueryProfile(PXCMFaceAnalysis.Attribute.Label.LABEL_EYE_CLOSED, 0, out attributeProfile);
+            attributeProfile.threshold = 50; //Must be here!
+            faceAttributes.SetProfile(PXCMFaceAnalysis.Attribute.Label.LABEL_EYE_CLOSED, ref attributeProfile);
+            attributeStatus = faceAttributes.QueryData(PXCMFaceAnalysis.Attribute.Label.LABEL_EYE_CLOSED, faceId, out blink);
+
+            faceAttributes.QueryProfile(PXCMFaceAnalysis.Attribute.Label.LABEL_GENDER, 0, out attributeProfile);
+            faceAttributes.SetProfile(PXCMFaceAnalysis.Attribute.Label.LABEL_GENDER, ref attributeProfile);
+            attributeStatus = faceAttributes.QueryData(PXCMFaceAnalysis.Attribute.Label.LABEL_GENDER, faceId, out gender);
+
+            faceAttributes.QueryProfile(PXCMFaceAnalysis.Attribute.Label.LABEL_AGE_GROUP, 0, out attributeProfile);
+            faceAttributes.SetProfile(PXCMFaceAnalysis.Attribute.Label.LABEL_AGE_GROUP, ref attributeProfile);
+            attributeStatus = faceAttributes.QueryData(PXCMFaceAnalysis.Attribute.Label.LABEL_AGE_GROUP, faceId, out age_group);
+
+            ShowAttributesOnForm();
+            
+            //Do the application events
+            try
+            {
+                Application.DoEvents(); //TODO: This should be avoided using a different thread, but how?
+            }
+            catch (AccessViolationException e)
+            {
+                //TODO: Handle exception!
+            }
+            return true;
+        }
+
+        public override void OnImage(PXCMImage image)
+        {
+            session = QuerySession();
+            image.QueryBitmap(session, out lastProcessedBitmap);
+            using (Graphics drawer = Graphics.FromImage(lastProcessedBitmap))
+            {
+                if (locationStatus != pxcmStatus.PXCM_STATUS_ITEM_UNAVAILABLE)
+                {
+                    drawer.DrawRectangle(new Pen(new SolidBrush(Color.Red), 1), new Rectangle(new Point((int)faceLocationData.rectangle.x, (int)faceLocationData.rectangle.y), new Size((int)faceLocationData.rectangle.w, (int)faceLocationData.rectangle.h)));
+                }
+
+                if (landmarkStatus != pxcmStatus.PXCM_STATUS_ITEM_UNAVAILABLE && faceLandmarkData != null)
+                {
+                    drawer.DrawLine(new Pen(new SolidBrush(Color.Red), 1), new Point((int)faceLandmarkData[0].position.x, (int)faceLandmarkData[0].position.y), new Point((int)faceLandmarkData[1].position.x, (int)faceLandmarkData[1].position.y));
+                    drawer.DrawLine(new Pen(new SolidBrush(Color.Red), 1), new Point((int)faceLandmarkData[2].position.x, (int)faceLandmarkData[2].position.y), new Point((int)faceLandmarkData[3].position.x, (int)faceLandmarkData[3].position.y));
+                    drawer.DrawLine(new Pen(new SolidBrush(Color.Red), 1), new Point((int)faceLandmarkData[4].position.x, (int)faceLandmarkData[4].position.y), new Point((int)faceLandmarkData[5].position.x, (int)faceLandmarkData[5].position.y));
+                    drawer.DrawRectangle(new Pen(new SolidBrush(Color.Red), 1), new Rectangle((int)faceLandmarkData[6].position.x - 2, (int)faceLandmarkData[6].position.y - 2, 4, 4));
+                }
+            }
+
+            //Show main image
+            recipient.Image = lastProcessedBitmap;
+        }
+
+        private void ShowAttributesOnForm()
+        {
+            if (blink[0] == 100)
+            {
+                parent.checkBox1.Checked = true;
+            }
+            else
+            {
+                parent.checkBox1.Checked = false;
+            }
+
+            if (smile[0] == 100)
+            {
+                parent.checkBox2.Checked = true;
+            }
+            else
+            {
+                parent.checkBox2.Checked = false;
+            }
+
+            if (gender[0] == 100)
+            {
+                parent.label2.Text = "Male";
+            }
+            else
+            {
+                parent.label2.Text = "Female";
+            }
+
+            parent.trackBar1.Value = (int)age_group[0];
+            parent.trackBar2.Value = (int)age_group[1];
+            parent.trackBar3.Value = (int)age_group[2];
+            parent.trackBar4.Value = (int)age_group[3];
+            parent.trackBar5.Value = (int)age_group[4];
+        }
+    }
+}
